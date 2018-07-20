6b2aeefbb5e9da1071f8bdd6f5f706b9bc7c20aa
alexsyrom <alexsyrom@yandex.ru>
2018-07-20 12:05:32 +0300

    smth

--- train.py
+++ train.py
@@ -78,13 +78,13 @@ if __name__ == "__main__":
       "a weight of 1).")
   flags.DEFINE_float("base_learning_rate", 0.01,
                      "Which learning rate to start with.")
-  flags.DEFINE_float("learning_rate_decay", 0.7,
+  flags.DEFINE_float("learning_rate_decay", 0.1,
                      "Learning rate decay factor to be applied every "
                      "learning_rate_decay_examples.")
-  flags.DEFINE_float("learning_rate_decay_examples", 1000 * 1024,
+  flags.DEFINE_float("learning_rate_decay_examples", 4000 * 1000,
                      "Multiply current learning rate by learning_rate_decay "
                      "every learning_rate_decay_examples.")
-  flags.DEFINE_integer("num_epochs", 7,
+  flags.DEFINE_integer("num_epochs", 10,
                        "How many passes to make over the dataset before "
                        "halting training.")
   flags.DEFINE_integer("max_steps", None,
@@ -433,7 +433,7 @@ class Trainer(object):
         is_chief=self.is_master,
         global_step=global_step,
         save_model_secs=15 * 60,
-        save_summaries_secs=120,
+        save_summaries_secs=10,
         saver=saver)
 
     logging.info("%s: Starting managed session.", task_as_string(self.task))
