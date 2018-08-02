dc6d02eea7fca0c38af46091d18587448c878c97
alexsyrom <alexsyrom@yandex.ru>
2018-08-03 01:26:51 +0300

    10 sh

--- video_level_models.py
+++ video_level_models.py
@@ -246,7 +246,7 @@ class THSModel(models.BaseModel):
     with tf.variable_scope("shortcut_layer"):
       net = in_layer
       net_list = list()
-      for i in range(3):
+      for i in range(10):
         net = slim.fully_connected(
             net, 1024, activation_fn=tf.nn.relu,
             trainable=trainable,
